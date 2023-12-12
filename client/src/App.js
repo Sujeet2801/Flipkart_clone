2133f47bea45b7f9e892d72c3c22e584b48a326d
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2023-12-12 06:28:52 +0000

    Updated

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+
+//components
+import Header from './components/header/Header';
+// import Home from './components/home/Home';
+// import DetailView from './components/details/DetailView';
+// import Cart from './components/cart/cart';
+
+// import DataProvider from './context/DataProvider';
+import { Box } from '@mui/material';
+
+import { BrowserRouter, Routes} from 'react-router-dom';
+
+function App() {
+  return (
+         <BrowserRouter> 
+         <Header/>
+          <Box style={{marginTop: 54}}> 
+            <Routes> 
+              {/* <Route path='/' element={<Home />} />
+              <Route path='/product/:id' element={<DetailView />} />
+              <Route path='/cart' element={<Cart/>}/> */}
+            </Routes>  
+         </Box> 
+         </BrowserRouter>
+  );
+}
+
+export default App;
